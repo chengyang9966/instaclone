be9a0bf981fe7ba41000d725ad87848dbda28091
cheng Yang <chengyang.ong@digi.com.my>
2022-04-20 19:57:21 +0800

    empty invoice table

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,6 +1,6 @@
 const express = require("express"),
   router = express.Router();
-const invoices = require("../db.json");
+const invoices = [];
 
 const sendEmail = require("../utils/sendgridEmail");
 const { base64_encode } = require("../utils/base64EnCode");
